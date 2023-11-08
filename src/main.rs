b9c0b05ee71200d03d7ee181272b0c9724bbf870
crawdaddie <adam.a.juraszek@googlemail.com>
2023-11-08 11:07:30 +0000

    add a few extern fns

--- src/main.rs
+++ src/main.rs
@@ -40,31 +40,21 @@ fn read_file_to_string(file_path: &str) -> Result<String, io::Error> {
     Ok(contents)
 }
 
-// extern "C" {
-//     fn printf(format: *const i8, ...) -> i32;
-// }
-// #[no_mangle]
-// pub unsafe extern "C" fn printf(fmt: *const i8) -> i32 {
-//     unsafe {
-//         println!("{:?}", fmt.as_ref());
-//     }
-//     1
-// }
-//
-//
-// macro used to print & flush without printing a new line
-// macro_rules! print_flush {
-//     ( $( $x:expr ),* ) => {
-//         print!( $($x, )* );
-//
-//         std::io::stdout().flush().expect("Could not flush to standard output.");
-//     };
-// }
-// #[no_mangle]
-// pub extern "C" fn putchard(x: f64) -> f64 {
-//     print_flush!("{}", x as u8 as char);
-//     x
-// }
+#[no_mangle]
+pub extern "C" fn ex(x: f64) -> f64 {
+    println!("hello extern {}", x);
+    x
+}
+#[used]
+static EX: extern "C" fn(f64) -> f64 = ex;
+
+#[no_mangle]
+pub extern "C" fn printf(fmt: *const u8) -> i64 {
+    println!("{:?}", fmt);
+    1
+}
+#[used]
+static PRINTF: extern "C" fn(*const u8) -> i64 = printf;
 
 fn main() -> Result<(), io::Error> {
     // Parse command-line arguments
@@ -72,16 +62,12 @@ fn main() -> Result<(), io::Error> {
 
     let file_contents = match args.code {
         true => args.input.unwrap(),
-        false if args.interactive => "".into(),
         false => read_file_to_string(args.input.unwrap().as_str())?,
     };
 
     let context = Context::create();
     let module = context.create_module("ylc");
     let builder = context.create_builder();
-    // let execution_engine = module
-    //     .create_jit_execution_engine(OptimizationLevel::None)
-    //     .unwrap();
 
     // Create FPM
     let fpm = PassManager::create(&module);
@@ -116,8 +102,6 @@ fn main() -> Result<(), io::Error> {
 
         let final_type = main_fn.get_type().get_return_type();
         match final_type {
-            // TODO: try to match the real llvm return type of the fn
-            // NB: compile_program sets this as a void_type by default for now
             Some(BasicTypeEnum::IntType(_)) => unsafe {
                 let compiled_fn = ee.get_function::<unsafe extern "C" fn() -> i64>(name.as_str());
                 println!("=> {:?}", compiled_fn.unwrap().call());
@@ -144,46 +128,37 @@ fn main() -> Result<(), io::Error> {
         // unsafe { ee.get_function::<unsafe extern "C" fn() -> i64>(name.as_str()) };
     }
 
-    // if codegen_program(program, &mut ctx).is_ok() {
-    //     ctx.module.print_to_stderr();
-    //     if let Some(main_fn) = ctx.get_function::<MainFunc>("main") {
-    //         unsafe {
-    //             main_fn.call();
-    //         }
-    //     }
-    // };
-
-    // println!("top-level env: {:?}", ctx.env.current().unwrap());
-
     // let ts_ctx = Arc::new(Mutex::new(ctx)); // unfortunately need to wrap
     // in Arc(Mutex(...)) because rust can't prove that the closure won't be passed to another
     // thread
 
-    // if args.interactive {
-    //     let _ = repl::repl(|line| {
-    //         let mut program = parser::parse(line);
-    //
-    //         infer_types(&mut program);
-    //
-    //         println!("\x1b[1;35m");
-    //         for s in &program {
-    //             // print_ast(s.clone(), 0);
-    //             println!("{:?}", s);
-    //         }
-    //         println!("\x1b[1;0m");
-    //
-    //         let mut ctx = ts_ctx.lock().unwrap();
-    //         if codegen_program(program, &mut ctx).is_ok() {
-    //             ctx.module.print_to_stderr();
-    //
-    //             if let Some(main_fn) = ctx.get_function::<MainFunc>("main") {
-    //                 unsafe {
-    //                     main_fn.call();
-    //                 }
-    //             }
-    //         }
-    //     });
-    // }
+    if args.interactive {
+        // println!("interactive");
+
+        // let _ = repl::repl(|line| {
+        //     let mut program = parser::parse(line);
+        //
+        //     infer_types(&mut program);
+        //
+        //     println!("\x1b[1;35m");
+        //     for s in &program {
+        //         // print_ast(s.clone(), 0);
+        //         println!("{:?}", s);
+        //     }
+        //     println!("\x1b[1;0m");
+        //
+        //     let mut ctx = ts_ctx.lock().unwrap();
+        //     if codegen_program(program, &mut ctx).is_ok() {
+        //         ctx.module.print_to_stderr();
+        //
+        //         if let Some(main_fn) = ctx.get_function::<MainFunc>("main") {
+        //             unsafe {
+        //                 main_fn.call();
+        //             }
+        //         }
+        //     }
+        // });
+    }
 
     Ok(())
 }
